b75314b3bad560440194d6dba1296a293697bb3c
GusthavoSoares <gusthavorsoares@gmail.com>
2024-06-12 23:13:10 -0300

    Criando o primeiro formulário

---
+++ view/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilos/principal.css">
+    <title>Cadastro de clientes</title>
+</head>
+<body>
+    <form class="formulario" method="post">
+        <h1>Atendimentos</h1>
+        <fieldset>
+            <legend>Identificação</legend>
+            <div class="entradaFormulario">
+                <label for="identificacaoAtendente">Identificação do Atendente</label>
+                <input type="text" id="identificacaoAtendente" value="identificacaoAtendente" required>
+            </div>
+            <div class="entradaFormulario">
+                <label for="identificacaoAtendente">Identificação do Atendente</label>
+                <select>
+                    <option></option>
+                    <option></option>
+                    <option></option>
+                </select>
+            </div>
+        </fieldset>
+
+    </form>
+    <script src="https://kit.fontawesome.com/df85906e6a.js" crossorigin="anonymous"></script>
+</body>
+</html>
