f5e2823c1f261ea5a70eb6d18cfbc0d7116d33b6
GusthavoSoares <gusthavorsoares@gmail.com>
2024-06-16 07:56:05 -0300

    Iniciando o projeto

--- view/index.php
+++ view/index.php
@@ -12,17 +12,50 @@
         <fieldset>
             <legend>Identificação</legend>
             <div class="entradaFormulario">
-                <label for="identificacaoAtendente">Identificação do Atendente</label>
-                <input type="text" id="identificacaoAtendente" value="identificacaoAtendente" required>
+                <label for="identificacaoAtendente">Nome do Atendente</label>
+                <input type="text" id="identificacaoAtendente" name="identificacaoAtendente" required minlength="5" maxlength="100">
             </div>
             <div class="entradaFormulario">
-                <label for="identificacaoAtendente">Identificação do Atendente</label>
-                <select>
-                    <option></option>
-                    <option></option>
-                    <option></option>
+                <label for="identificacaoAtendente">Tipo de atendimento</label>
+                <select name="tipoAtendimento" required>
+                    <option value="0">Escolha o tipo de atendimento...</option>
+                    <option value="1">Presencial</option>
+                    <option value="2">Whatsapp</option>
+                    <option value="3">Ligação telefônica</option>
+                    <option value="4">E-mail</option>
+                    <option value="5">Redes sociais</option>
+                    <option value="6">Teams</option>
+                    <option value="7">Outra</option>
                 </select>
             </div>
+            <div class="entradaFormulario">
+                <label for="pessoaAtendida">Quem estou atendendo </label>
+                <div class="opcaoFormulario">
+                    <input type="radio" name="pessoasAtendimento" id="empregador" required>
+                    <label for="empregador">Empregador</label>
+                </div>
+                <div class="opcaoFormulario">
+                    <input type="radio" name="pessoasAtendimento" id="trabalhador" required>
+                    <label for="trabalhador">Trabalhador</label>
+                </div>
+                <div class="opcaoFormulario">
+                    <input type="radio" name="pessoasAtendimento" id="ads" required> 
+                    <label for="ads">ADS</label> 
+                </div>
+                <div class="opcaoFormulario">
+                    <input type="radio" name="pessoasAtendimento" id="setores" required>
+                    <label for="setores">Setores da FGTAS</label>
+                </div>
+                <div class="opcaoFormulario">
+                    <input type="radio" name="pessoasAtendimento" id="mercadoTrabalho" required>
+                    <label for="mercadoTrabalho">Interessado em informações sobre o Mercado de trabalho</label>
+                </div>
+                <div class="opcaoFormulario">
+                    <input type="radio" name="pessoasAtendimento" id="outro" required>
+                    <label for="outro">Outro</label>
+                </div>
+            </div>
+            <input type="submit" value="Próximo">
         </fieldset>
 
     </form>
