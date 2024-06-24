53bf9a121f580e9260b45b4cc62ff5f4b84cb5b5
GusthavoSoares <gusthavorsoares@gmail.com>
2024-06-23 22:35:12 -0300

    Página de login

--- view/index.php
+++ view/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./estilos/geral.css">
-    <link rel="stylesheet" href="./estilos/paginaPrincipal.css">
-    <title>Cadastro de clientes</title>
+    <link rel="stylesheet" href="./estilos/principal.css">
+    <title>Cadastro de Atendimentos</title>
 </head>
 <body>
     <form class="formulario" method="post">
@@ -62,6 +62,7 @@
         </fieldset>
 
     </form>
+    
     <script src="https://kit.fontawesome.com/df85906e6a.js" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ view/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilos/geral.css">
+    <link rel="stylesheet" href="./estilos/principal.css">
+    <link rel="stylesheet" href="./estilos/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <form class="formulario w50 h25" method="post">
+        <h1>Entrar</h1>
+        
+            <div class="formulario__login">
+                <label class="formulario__legenda centralizado mbt1" for="login">Login</label>
+                <input type="email" id="login" name="login" class="formulario__entrada mlr1" required minlength="5" maxlength="100">
+            </div>
+            <div class="formulario__login">
+                <label class="formulario__legenda centralizado  mbt1"  for="senha">Senha</label>
+                <div class="grid90_10">
+                    <input type="password" id="senha" name="senha" class="formulario__entrada mlr1 relativo" required minlength="5" maxlength="20">
+                    <i class="fa-solid fa-eye icone mt1" id="olho"></i>
+                </div>
+            </div>
+            <div class="centralizado">
+                <input type="submit" value="Logar" class="formulario__botao">
+            </div>
+
+    </form>
+    <script src="https://kit.fontawesome.com/df85906e6a.js" crossorigin="anonymous"></script>
+    <script src="./js/olho.js"></script>
+</body>
+</html>
