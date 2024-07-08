125cbc372e3787be3a7d3c66475f3bbaf75163d4
JonasSJ <89933465+JonasSJesus@users.noreply.github.com>
2024-07-08 01:32:45 -0300

    Update cadastrar_usuario.php

--- view/cadastrar_usuario.php
+++ view/cadastrar_usuario.php
@@ -11,20 +11,27 @@
     <title>Cadastrar Usuário</title>
 </head>
 <body>
-    <form method="post" class="formulario mt12-5 centralizado">
+    <form method="post" class="formulario mt12-5 centralizado" action="../src/controller/cadastraUsuario.php">
         <fieldset class="formulario__campo w80">
             <legend class="formulario__subtitulo">Usuário</legend>
             <section class="centralizado">
                 <label class="formulario__etiqueta" for="nomeUsuario">Nome</label>
                 <div>
-                    <input type="text" class="formulario__entrada w90" value='' name="nomeUsuario" required id="nomeUsuario" placeholder="João da Silva"  >
+                    <input type="text" class="formulario__entrada w90" value='' name="nome" required id="nomeUsuario" placeholder="João da Silva"  >
                 </div>
             </section>
             <section class="centralizado mv1">
                 <label class="formulario__etiqueta" for="emailUsuario ">E-mail</label>
                 <div>
-                    <input class="formulario__entrada w90" type="email" value='' name="emailUsuario" required id="emailUsuario"
+                    <input class="formulario__entrada w90" type="email" value='' name="email" required id="emailUsuario"
                     placeholder="exemploEmail@gmail.com" >
+                </div>
+            </section>
+            <section>
+                <label class="formulario__etiqueta"  for="senha">Senha</label>
+                <div class="mv0-5">
+                    <input value="" type="password" id="senha" name="senha" class="formulario__entrada w80" required minlength="5" maxlength="20">
+                    <i class="fa-solid fa-eye icone centralizado mh1" id="olho"></i>
                 </div>
             </section>
             <section>
@@ -39,7 +46,7 @@
             <fieldset class="formulario__campo w80 mt2">
             <legend class="formulario__subtitulo">Ativo</legend>
                 <div class="centralizado mv0-5">
-                    <select class="formulario__selecao w90" required name="ativo">
+                    <select class="formulario__selecao w90" required name="ativoUsuario">
                         <option value="Ativado">Ativado</option>
                         <option value="Desativado">Desativado</option>
                     </select>
@@ -48,9 +55,10 @@
         </fieldset>
         <div class="w80 centralizado mv0-5">
             <a href="admin.php" class="formulario__botao__secundario formulario__botao__padrao">Voltar</a>
-            <input type="submit" class="formulario__botao formulario__botao__padrao" value="Cadastrar">
+            <input type="submit" class="formulario__botao formulario__botao__padrao" value="cadastrar" name="salvar">
         </div>
     </form>
 <script src="https://kit.fontawesome.com/df85906e6a.js" crossorigin="anonymous"></script>
+<script src="./js/olho.js"></script>
 </body>
 </html>
