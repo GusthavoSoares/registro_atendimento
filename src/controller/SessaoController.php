95ffe437735ddab82392199fc8575439ca2800d7
GusthavoSoares <gusthavorsoares@gmail.com>
2024-07-16 23:21:12 -0300

    Descomentando o sessão controller

--- src/controller/SessaoController.php
+++ src/controller/SessaoController.php
@@ -3,11 +3,10 @@
 class SessaoController
 {
     public function protecao(): void
-    {
-        
-        // session_start();
-        // if(!array_key_exists('logado', $_SESSION) || $_SESSION['logado'] == false){
-        //     header("location: login.php");
-        // }
+    { 
+        session_start();
+        if(!array_key_exists('logado', $_SESSION) || $_SESSION['logado'] == false){
+            header("location: login.php");
+        }
     }
 }
